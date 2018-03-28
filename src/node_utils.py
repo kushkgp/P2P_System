921eced7429b5390a0abe5c51635c97ae0fa6c93
kushagraaa <gulabarora07@gmail.com>
2018-03-28 18:12:41 +0530

    asdfasdf

--- src/node_utils.py
+++ src/node_utils.py
@@ -13,10 +13,10 @@ def get_hublist():
 	hublist = recvTCP(s)
 	return hublist
 
-def connect_hub(ip, aggregateQHT, isLeaf):
+def connect_hub(ip, a, isLeaf):
 	addr = (ip, HUB_TCP_PORT)
 	s = initTCPSocket(addr)
-	sendTCP(s, ("updateQHT",aggregateQHT, isLeaf))
+	sendTCP(s, ("updateQHT",a.get_aggregateQHT(), isLeaf))
 	a.neighbours[ip] = recvTCP(s)
 	s.close()
 
@@ -27,11 +27,12 @@ def joinCluster(a, CLUSTER_LIMIT, isLeaf):
 		x = len(a.neighbours)
 		for hub in a.hublist:
 			try:
-				connect_hub(hub, a.get_aggregateQHT(), isLeaf)
+				connect_hub(hub, a, isLeaf)
 				x+=1
 				if x >= CLUSTER_LIMIT:
 					break
 			except Exception as e:
+				print e.message
 				continue
 	except Exception as e:
 		print e.message
